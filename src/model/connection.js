445670e58c3cdb2c5fa73ac228a9c67d20140112
Guilherme Fonseca <guii.ffonseca22@gmail.com>
2022-03-05 14:39:38 -0300

    [feat] Create connection with MongoClient

---
+++ src/model/connection.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require("mongodb");
+
+const URL = 'mongodb://127.0.0.1:27017'
+const DB_NAME = 'RomanCalculator';
+const OPTIONS = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+let db = null;
+
+const connection = () => {
+    return db
+    ? Promise.resolve(db)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => {
+    db = conn.db('model_example');
+    return db;
+    })
+};
+
+module.exports = connection;
