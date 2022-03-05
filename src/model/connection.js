1098e8e0f8d9f23ddd3f50bf9fa81afd36047428
Guilherme Fonseca <guii.ffonseca22@gmail.com>
2022-03-05 15:10:14 -0300

    [fix] URL and DB_NAME fixed

--- src/model/connection.js
+++ src/model/connection.js
@@ -12,9 +12,9 @@ let db = null;
 const connection = () => {
     return db
     ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    : MongoClient.connect(URL, OPTIONS)
     .then((conn) => {
-    db = conn.db('model_example');
+    db = conn.db(DB_NAME);
     return db;
     })
 };
